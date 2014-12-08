4917ca5e25cead91f7967667fdf1cf72d5dd5e73
lfreitas <luiz.penna.freitas@gmail.com>
2014-12-07 20:04:50 -0500

    Fixed typos, spacing

--- webhose-io/webhose_analytics.R
+++ webhose-io/webhose_analytics.R
@@ -2,7 +2,7 @@
 # allows you to query the past 72 hours of activity across "hundreds of thousands 
 # of forums, news and blogs"
 
-# This script is designed to query instragram.com, but webhose's API offers tons of 
+# This script is designed to query instagram.com, but webhose's API offers tons of 
 # flexibility. Have a look at the documentation to explore more specific or complex
 # queries.
 
@@ -18,10 +18,8 @@ URL <- paste("https://webhose.io/search?token=",apikey,"&format=json&q=",keyword
 data <- fromJSON(getURL(URL,.opts = list(ssl.verifypeer = FALSE)))
 posts <- data[1]
 
-
-
-# The following lines use some of the returned properties to page through the 
-# available results
+# the following lines use some of the properties returned by Webhose to page through 
+# the available results
 if (data$moreResultsAvailable==0){
   print("Nothing else to get.")
 } else while (data$moreResultsAvailable>=1){
@@ -32,8 +30,6 @@ if (data$moreResultsAvailable==0){
 }
 print("Done getting results.")
 
-
-
 # convert the list to a data frame
 df <- NULL
 for (i in 1:length(posts)){
@@ -42,12 +38,13 @@ for (i in 1:length(posts)){
   }
 }
 
-# replace unreadable characters with a question mark
+# replace unreadable characters with a question mark to avoid a lot of encoding frustration
 df$text<-iconv(df$text,from="UTF-8",to="ASCII",sub="?")
 
+# lower-case, remove punctuation, numbers, and standard stopwords, and normalize whitespace
+# the call using PlainTextDocument circumvents some variable typing issues with TDM and DTM
 doc.vec <- VectorSource(as.vector(df$text))
 doc.corpus <- Corpus(doc.vec)
-# summary(doc.corpus)
 doc.corpus <- tm_map(doc.corpus, tolower)
 doc.corpus <- tm_map(doc.corpus, removePunctuation)
 doc.corpus <- tm_map(doc.corpus, removeNumbers)
@@ -55,14 +52,16 @@ doc.corpus <- tm_map(doc.corpus, removeWords, stopwords("english"))
 doc.corpus <- tm_map(doc.corpus, stripWhitespace)
 doc.corpus <- tm_map(doc.corpus, PlainTextDocument)
 
+# pull a random document to see how it looks
+# run this repeatedly to see a random sample of documents
 inspect(doc.corpus[sample(1:length(df$text),1)])
 
 # build both the term-document document-term matrices
 TDM <- TermDocumentMatrix(doc.corpus)
 DTM <- DocumentTermMatrix(doc.corpus)
 
-# list some of the most frequent terms, arbitrarily defined at appearing 10 or more times
+# list some of the most frequent terms, arbitrarily defined as appearing 10 or more times
 findFreqTerms(TDM, lowfreq=10)
 
-# show the degree of association between the keyword defining the analysis and other terms, limited to an arbitrary associated of 0.29 or greater
+# show the degree of association between the keyword defining the analysis and other terms, limited to an arbitrary association of 0.29 or greater
 findAssocs(TDM, keyword, 0.29)
