54195a36bfb8ee36b3f2bf308ba63fb7dc8d4409
lfreitas <luiz.penna.freitas@gmail.com>
2015-01-17 02:36:14 -0500

    API paging debugged

--- cambridge-parking/cambridge-parking.R
+++ cambridge-parking/cambridge-parking.R
@@ -3,11 +3,41 @@
 
 library(RCurl)
 library(jsonlite)
+library(httr)
+library(dplyr)
 
 # grab data from Cambridge data service
-data <- getURL("https://data.cambridgema.gov/resource/vnxa-cuyr.json",.opts = list(ssl.verifypeer = FALSE))
-df0 <- fromJSON(data)
-df1 <- flatten(df0)
+
+data <- list()
+offset <- 0
+get <- 1
+
+if (length(get)==0){
+  message("Nothing else to get.")
+} else while (length(get)>0) {
+  message(paste("Fetching at offset of",offset))
+  url <- getURL(paste("https://data.cambridgema.gov/resource/vnxa-cuyr.json?$order=:id&$limit=25000&$offset=",as.integer(offset),sep=""),.opts = list(ssl.verifypeer = FALSE))
+  get <- tryCatch({
+    
+    flatten(fromJSON(url))
+    
+    }, error=function(err) {
+      
+    return(NULL)
+    
+  })
+  data[[length(data)+1]] <- get
+  offset = offset + 25000
+  
+  #if (offset >75000) { break }
+}
+message("Done getting results.")
+
+
+#for (i in 1:length(data)){print(head(data[[i]]$ticket_issue_date))}
+#for (i in 1:length(data)){print(colnames(data[[i]]))}
+
+df1 <- bind_rows(data)
 
 # additional processing for addresses, since they are nested in their own JSON
 addresses <- t(sapply(df1$location.human_address, fromJSON))
